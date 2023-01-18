6de5d03009eb08d68d8c32471ec5cb15b7724a76
chlalswns200 <chlalswns200@naver.com>
2023-01-18 09:21:45 +0900

    first commit

---
+++ src/main/java/com/example/branchexercise/BranchExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.example.branchexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchExerciseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BranchExerciseApplication.class, args);
+    }
+
+}
